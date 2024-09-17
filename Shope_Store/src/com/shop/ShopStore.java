35e8b984e6a8a0c0d753e94caf5bc26f09cf6df9
JokerGyiLay <152058827+JokerGyiLay@users.noreply.github.com>
2024-09-17 21:32:45 +0900

    Delete Shope_Store directory

--- Shope_Store/src/com/shop/ShopStore.java
+++
@@ -1,9 +0,0 @@
-package com.shop;
-
-import com.shop.controller.ShopController;
-
-public class ShopStore {
-        public static void main(String[] args) {
-	         new ShopController().start();
-    }
-}
